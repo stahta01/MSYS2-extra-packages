5f69b2e7fdeb9b5007503ff933bbb1e811ac0e9a
Tim Stahlhut <stahta01@gmail.com>
2024-12-27 16:20:59 -0500

    Update copy-xroar-depends-packages.sh

--- copy-xroar-depends-packages.sh
+++ copy-xroar-depends-packages.sh
@@ -8,7 +8,7 @@ cp -f ./flac/flac-1.4.3-2-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit
 cp -f ./lame/lame-3.100-5-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 cp -f ./libogg/libogg-1.3.5-2-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 cp -f ./opus/opus-1.5.2-1-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
-cp -f ./mpg123/mpg123-1.32.7-1-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
+cp -f ./mpg123/mpg123-1.32.7-2-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 cp -f ./libvorbis/libvorbis-1.3.7-3-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 
 mkdir -p packages/xroar_depends_MSYS_32bit_packages
@@ -21,5 +21,5 @@ cp -f ./flac/flac-1.4.3-2-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_p
 cp -f ./lame/lame-3.100-5-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
 cp -f ./libogg/libogg-1.3.5-2-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
 cp -f ./opus/opus-1.5.2-1-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
-cp -f ./mpg123/mpg123-1.32.7-1-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
+cp -f ./mpg123/mpg123-1.32.7-2-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
 cp -f ./libvorbis/libvorbis-1.3.7-3-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
