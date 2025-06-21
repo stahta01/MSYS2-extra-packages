6895e889358b6f44b274c2bdc640950580561b4d
Tim Stahlhut <stahta01@gmail.com>
2025-06-21 09:36:57 -0400

    script: Add copy-gtk3-and-depends-packages.sh

---
+++ copy-gtk3-and-depends-packages.sh
@@ -0,0 +1,57 @@
+mkdir -p packages/gtk3_and_depends_MSYS_64bit_packages
+
+rm -f ./packages/gtk3_and_depends_MSYS_64bit_packages/*.zst
+cp --preserve=timestamps -f ./libpng/libpng-1.6.44-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./freetype/freetype-2.13.3-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./fontconfig/fontconfig-2.15.0-2-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./pixman/pixman-0.43.4-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./graphite2/graphite2-1.3.14-4-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./cairo/cairo-1.18.2-3-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./harfbuzz/harfbuzz-10.1.0-2-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./fribidi/fribidi-1.0.16-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./libdatrie/libdatrie-0.2.13-3-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./libthai/libthai-0.1.29-3-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./pango/pango-1.54.0-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./at-spi2-core/atk-2.56.2-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./egl-headers/egl-headers-1.5.r255.9ab6036-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./gles-headers/gles-headers-3.2.r1005.d127c30-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./libepoxy/libepoxy-1.5.10-5-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./shared-mime-info/shared-mime-info-2.4-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./json-glib/json-glib-1.10.6-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./libjpeg-turbo/libjpeg-turbo-3.0.4-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./libtiff/libtiff-4.7.0-2-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./gdk-pixbuf2/gdk-pixbuf2-2.42.12-4-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./hicolor-icon-theme/hicolor-icon-theme-0.18-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./gtk3/gtk-update-icon-cache-3.24.49-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./adwaita-icon-theme-legacy/adwaita-icon-theme-legacy-46.2-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./adwaita-icon-theme/adwaita-icon-theme-48.0-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/ 
+cp --preserve=timestamps -f ./gtk3/gtk3-3.24.49-1-x86_64.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_64bit_packages/
+
+mkdir -p packages/gtk3_and_depends_MSYS_32bit_packages
+
+rm -f ./packages/gtk3_and_depends_MSYS_32bit_packages/*.zst
+cp --preserve=timestamps -f ./libpng/libpng-1.6.44-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./freetype/freetype-2.13.3-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./fontconfig/fontconfig-2.15.0-2-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./pixman/pixman-0.43.4-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./graphite2/graphite2-1.3.14-4-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./cairo/cairo-1.18.2-3-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./harfbuzz/harfbuzz-10.1.0-2-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./fribidi/fribidi-1.0.16-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./libdatrie/libdatrie-0.2.13-3-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./libthai/libthai-0.1.29-3-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./pango/pango-1.54.0-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./at-spi2-core/atk-2.56.2-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./egl-headers/egl-headers-1.5.r255.9ab6036-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./gles-headers/gles-headers-3.2.r1005.d127c30-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./libepoxy/libepoxy-1.5.10-5-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./shared-mime-info/shared-mime-info-2.4-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./json-glib/json-glib-1.10.6-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./libjpeg-turbo/libjpeg-turbo-3.0.4-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./libtiff/libtiff-4.7.0-2-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./gdk-pixbuf2/gdk-pixbuf2-2.42.12-4-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./hicolor-icon-theme/hicolor-icon-theme-0.18-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./gtk3/gtk-update-icon-cache-3.24.49-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./adwaita-icon-theme-legacy/adwaita-icon-theme-legacy-46.2-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./adwaita-icon-theme/adwaita-icon-theme-48.0-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/ 
+cp --preserve=timestamps -f ./gtk3/gtk3-3.24.49-1-i686.pkg.tar.zst ./packages/gtk3_and_depends_MSYS_32bit_packages/ 
