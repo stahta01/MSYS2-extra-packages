2b152480e0acb40790d896a4f78616a244af7dd8
Tim Stahlhut <stahta01@gmail.com>
2026-04-29 19:25:42 -0400

    script: Update copy-xroar-depends-packages.sh

--- copy-xroar-depends-packages.sh
+++ copy-xroar-depends-packages.sh
@@ -2,24 +2,24 @@ mkdir -p packages/xroar_depends_MSYS_64bit_packages
 
 rm -f ./packages/xroar_depends_MSYS_64bit_packages/*.zst
 cp --preserve=timestamps -f ./libogg/libogg-1.3.5-4-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./sdl2/sdl2-2.30.8-2-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 cp --preserve=timestamps -f ./libpng/libpng-1.6.44-1-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 cp --preserve=timestamps -f ./flac/flac-1.4.3-3-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 cp --preserve=timestamps -f ./lame/lame-3.100-6-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 cp --preserve=timestamps -f ./opus/opus-1.5.2-1-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
-cp --preserve=timestamps -f ./mpg123/mpg123-1.32.7-2-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 cp --preserve=timestamps -f ./libvorbis/libvorbis-1.3.7-3-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./sdl2/sdl2-2.32.10-1-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
+cp --preserve=timestamps -f ./mpg123/mpg123-1.33.5-1-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 cp --preserve=timestamps -f ./libsndfile/libsndfile-1.2.2-3-x86_64.pkg.tar.zst ./packages/xroar_depends_MSYS_64bit_packages/
 
 mkdir -p packages/xroar_depends_MSYS_32bit_packages
 
 rm -f ./packages/xroar_depends_MSYS_32bit_packages/*.zst
 cp --preserve=timestamps -f ./libogg/libogg-1.3.5-4-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./sdl2/sdl2-2.30.8-2-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
 cp --preserve=timestamps -f ./libpng/libpng-1.6.44-1-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
 cp --preserve=timestamps -f ./flac/flac-1.4.3-3-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
 cp --preserve=timestamps -f ./lame/lame-3.100-6-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
 cp --preserve=timestamps -f ./opus/opus-1.5.2-1-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
-cp --preserve=timestamps -f ./mpg123/mpg123-1.32.7-2-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
 cp --preserve=timestamps -f ./libvorbis/libvorbis-1.3.7-3-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./sdl2/sdl2-2.32.10-1-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
+cp --preserve=timestamps -f ./mpg123/mpg123-1.33.5-1-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
 cp --preserve=timestamps -f ./libsndfile/libsndfile-1.2.2-3-i686.pkg.tar.zst ./packages/xroar_depends_MSYS_32bit_packages/
